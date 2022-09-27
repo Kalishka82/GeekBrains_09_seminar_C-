1d1b0cec0877d709e9a7ad79de611e9552a78b10
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-27 22:18:28 +0300

    add ex 63 Natural Row Until Number

---
+++ ex_63/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+// промежутке от 1 до N.
+
+using System;
+Console.Clear();
+
+Console.Write("Input integer number: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetNaturalRow(N));
+
+string GetNaturalRow (int N)
+{
+      if (N == 1) 
+            return "1";
+      
+      return GetNaturalRow(N - 1) + ", " + N;
+}
