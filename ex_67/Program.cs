3784aaae189df729500b109f69630a47a6df4710
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-27 22:21:39 +0300

    add ex 67 Sum of Num Digits

---
+++ ex_67/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.using System;
+using System;
+Console.Clear();
+
+Console.Write("Input integer number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetNumbersSum(number));
+
+// возвращает сумму цифр
+int GetNumbersSum(int number) 
+{
+      int sum = 0;
+      if (number == 0)
+            return number % 10;     // возвращает остаток от деления (последний разряд)
+
+      return GetNumbersSum(number / 10) + number % 10;
+}
